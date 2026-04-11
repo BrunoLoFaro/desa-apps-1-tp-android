71758ab65ed5bc09d030e56eb4cddeb3d2129701
marlene.n.benitez <marlene.n.benitez@accenture.com>
2026-04-11 08:17:34 -0300

    loader

--- app/src/main/java/com/example/myapplication/ui/home/viewmodel/HomeViewModel.java
+++ app/src/main/java/com/example/myapplication/ui/home/viewmodel/HomeViewModel.java
@@ -21,6 +21,8 @@ public class HomeViewModel extends ViewModel {
     private final MutableLiveData<List<TourActivity>> _featuredTours = new MutableLiveData<>();
     private final MutableLiveData<List<TourActivity>> _allTours = new MutableLiveData<>();
     private final MutableLiveData<UiMessage> _error = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> _loading = new MutableLiveData<>(true);
+    private int pendingCalls = 0;
 
     @Inject
     public HomeViewModel(SessionRepository sessionRepository, TourRepository tourRepository) {
@@ -32,6 +34,7 @@ public class HomeViewModel extends ViewModel {
     public LiveData<List<TourActivity>> getFeaturedTours() { return _featuredTours; }
     public LiveData<List<TourActivity>> getAllTours() { return _allTours; }
     public LiveData<UiMessage> getError() { return _error; }
+    public LiveData<Boolean> isLoading() { return _loading; }
 
     public boolean hasValidSession() {
         return sessionRepository.hasValidSession();
@@ -42,15 +45,20 @@ public class HomeViewModel extends ViewModel {
     }
 
     private void loadTours() {
+        pendingCalls = 2;
+        _loading.setValue(true);
+
         tourRepository.getFeaturedTours(new RepositoryCallback<List<TourActivity>>() {
             @Override
             public void onSuccess(List<TourActivity> data) {
                 _featuredTours.setValue(data);
+                onCallFinished();
             }
 
             @Override
             public void onError(UiMessage error) {
                 _error.setValue(error);
+                onCallFinished();
             }
         });
 
@@ -58,13 +66,22 @@ public class HomeViewModel extends ViewModel {
             @Override
             public void onSuccess(List<TourActivity> data) {
                 _allTours.setValue(data);
+                onCallFinished();
             }
 
             @Override
             public void onError(UiMessage error) {
                 _error.setValue(error);
+                onCallFinished();
             }
         });
+    }
+
+    private void onCallFinished() {
+        pendingCalls--;
+        if (pendingCalls <= 0) {
+            _loading.setValue(false);
+        }
     }
 
     @Override
